e699b51db7e59b22695aaa112d470428c312b444
Mufaddal <muffi94@gmail.com>
2017-02-01 05:07:44 +0500

    Tooltips for NavBar

--- src/components/NavBarButtons.jsx
+++ src/components/NavBarButtons.jsx
@@ -2,6 +2,7 @@ import React, {PropTypes} from 'react';
 import Radium from "radium";
 import {Link} from "react-router"
 import {Style} from "radium"
+import {OverlayTrigger,Tooltip} from 'react-bootstrap'
 var styles = {
 
     "padding": "1rem",
@@ -15,13 +16,15 @@ var styles = {
 class NavBarButtons extends React.Component {
 
     render() {
+      const tooltip = (<Tooltip id={'tooltip_'+this.props.text}>{this.props.tooltip}</Tooltip>)
         return (
             <div>
-
+              <OverlayTrigger placement='right' overlay={tooltip}>
                 <div className="button" style={styles}>
                     <span><img src={this.props.icon} width="16px" height="20px"></img></span><Link
                     to={this.props.link}>{this.props.text}</Link>
                 </div>
+                </OverlayTrigger>
             </div>
         );
     }
